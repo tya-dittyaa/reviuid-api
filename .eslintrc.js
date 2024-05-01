078293a1dc9fbb5bd09e10d5d80968f623fff181
Aditya Fajri <adityafajriisnandar@gmail.com>
2024-05-01 12:01:01 +0700

    fix: eslint error with prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,5 @@
+const e = require('express');
+
 module.exports = {
   parser: '@typescript-eslint/parser',
   parserOptions: {
@@ -21,5 +23,6 @@ module.exports = {
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
+    'prettier/prettier': ['error', { endOfLine: 'auto' }],
   },
 };
