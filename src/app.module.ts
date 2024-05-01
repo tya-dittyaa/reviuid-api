8a55c165b03ef7a9e929313e17642b8514e9f256
Aditya Fajri <adityafajriisnandar@gmail.com>
2024-05-01 15:56:40 +0700

    refactor: remove PrismaService

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,8 @@
 import { Module } from '@nestjs/common';
-import { PrismaService } from './prisma/prisma.service';
 
 @Module({
   imports: [],
   controllers: [],
-  providers: [PrismaService],
+  providers: [],
 })
 export class AppModule {}
