7196d72b55a6b139f41d7432671c26800282c86b
Aditya Fajri <adityafajriisnandar@gmail.com>
2024-05-01 20:47:31 +0700

    feat: Remove csurf middleware

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,4 @@
 import { NestFactory } from '@nestjs/core';
-import * as csurf from 'csurf';
 import helmet from 'helmet';
 import { AppModule } from './app.module';
 
@@ -7,8 +6,6 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
   app.use(helmet());
-  app.use(csurf());
-  await app.listen(process.env.API_PORT);
+  await app.listen(4000);
 }
-
 bootstrap();
