8645af2e2a1e377947f7a94d478fd76c01b89b33
Aditya Fajri <adityafajriisnandar@gmail.com>
2024-05-01 12:01:49 +0700

    feat: adding security

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
+import * as csurf from 'csurf';
+import helmet from 'helmet';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
+  app.use(helmet());
+  app.use(csurf());
   await app.listen(3000);
 }
 bootstrap();
