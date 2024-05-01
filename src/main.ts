e622e2b57fc26eb73d36f62fbb07ce5f3ebe2a0e
Aditya Fajri <adityafajriisnandar@gmail.com>
2024-05-01 21:51:58 +0700

    refactor: Add @nestjs/swagger package and configure Swagger documentation in main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,24 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import helmet from 'helmet';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const port = parseInt(process.env.API_PORT);
+
   app.enableCors();
   app.use(helmet());
-  await app.listen(4000);
+
+  const config = new DocumentBuilder()
+    .setTitle('Reviu.ID API Documentation')
+    .setDescription('The Reviu.ID API description for developers')
+    .setVersion('0.0.1')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(port);
 }
 bootstrap();
