2c71ce940517edeb6f53ed37b3b175585650c18d
Aditya Fajri <adityafajriisnandar@gmail.com>
2024-05-02 09:17:36 +0700

    Add authentication module and related files

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,8 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { AuthModule } from './auth/auth.module';
 import configuration from './config/configuration';
 import { UsersModule } from './users/users.module';
-import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
