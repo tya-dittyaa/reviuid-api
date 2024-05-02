f20365ea0626295b30a0da2b278f4e1e9693ecb6
Aditya Fajri <adityafajriisnandar@gmail.com>
2024-05-02 07:58:22 +0700

    Refactor user service to encrypt passwords

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,5 +1,6 @@
-import { HttpException, Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { Prisma } from '@prisma/client';
+import { randomBytes, scryptSync } from 'crypto';
 import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
@@ -11,8 +12,19 @@ export class UsersService {
       where: { OR: [{ username: data.username }, { email: data.email }] },
     });
 
-    if (user) throw new HttpException('The input data is already exist', 409);
+    if (user)
+      throw new HttpException(
+        'The input data is already exist',
+        HttpStatus.CONFLICT,
+      );
 
-    return this.prisma.user.create({ data });
+    data.password = this.encryptPassword(data.password);
+    await this.prisma.user.create({ data });
+  }
+
+  private encryptPassword(password: string) {
+    const salt = randomBytes(16).toString('hex');
+    const hash = scryptSync(password, salt, 64).toString('hex');
+    return `${salt}:${hash}`;
   }
 }
