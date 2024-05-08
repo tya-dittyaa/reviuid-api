8abbc9ceaecd5c96907206a7a7c130525e80f583
Aditya Fajri <adityafajriisnandar@gmail.com>
2024-05-08 14:23:12 +0700

    chore: Remove unused profile endpoint in UsersController

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,20 +1,9 @@
-import { Controller, Get, HttpCode, HttpStatus } from '@nestjs/common';
+import { Controller } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
-import { GetCurrentUserId } from 'src/common/decorators';
-import { PrismaService } from 'src/prisma/prisma.service';
 import { UsersService } from './users.service';
 
 @ApiTags('Users Endpoints')
 @Controller('users')
 export class UsersController {
-  constructor(
-    private readonly prisma: PrismaService,
-    private readonly usersService: UsersService,
-  ) {}
-
-  @Get('profile')
-  @HttpCode(HttpStatus.OK)
-  profile(@GetCurrentUserId() userId: string) {
-    return this.usersService.findOne(userId);
-  }
+  constructor(private readonly usersService: UsersService) {}
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -4,13 +4,4 @@ import { PrismaService } from 'src/prisma/prisma.service';
 @Injectable()
 export class UsersService {
   constructor(private prisma: PrismaService) {}
-
-  async findOne(userId: string) {
-    const user = await this.prisma.user.findUnique({
-      where: { id: userId },
-      select: { username: true, email: true, biography: true },
-    });
-
-    return { ...user, avatar: null };
-  }
 }
