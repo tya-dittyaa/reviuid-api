a8a33b8e984ab747290e91fda1f43d451adf3023
Aditya Fajri <adityafajriisnandar@gmail.com>
2024-05-09 21:40:16 +0700

    chore: Add getUserByUsername endpoint to UsersController

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,9 +1,17 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, HttpCode, HttpStatus, Param } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
+import { Public } from 'src/common/decorators';
 import { UsersService } from './users.service';
 
 @ApiTags('Users Endpoints')
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
+
+  @Public()
+  @Get(':username')
+  @HttpCode(HttpStatus.OK)
+  async getUserByUsername(@Param('username') username: string) {
+    return this.usersService.getUserByUsername(username);
+  }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,7 +1,25 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
 export class UsersService {
   constructor(private prisma: PrismaService) {}
+
+  async getUserByUsername(username: string) {
+    const user = await this.prisma.user.findUnique({
+      where: {
+        username,
+      },
+      select: {
+        username: true,
+        biography: true,
+      },
+    });
+
+    if (!user) {
+      throw new NotFoundException('User not found');
+    }
+
+    return user;
+  }
 }
