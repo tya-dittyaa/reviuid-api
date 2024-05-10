4987933556df5e7f41395550083216e96f9e9e82
Aditya Fajri <adityafajriisnandar@gmail.com>
2024-05-10 21:19:58 +0700

    feat: Add dependency injection for Prisma service in FilmsController and FilmsService

--- src/films/films.controller.ts
+++ src/films/films.controller.ts
@@ -1,4 +1,9 @@
 import { Controller } from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
+import { FilmsService } from './films.service';
 
+@ApiTags('Films Endpoints')
 @Controller('films')
-export class FilmsController {}
+export class FilmsController {
+  constructor(private readonly filmsService: FilmsService) {}
+}
