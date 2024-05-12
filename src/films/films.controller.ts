64a254d151f0cf7a6708bada48b8d07345058e24
Aditya Fajri <adityafajriisnandar@gmail.com>
2024-05-12 20:28:20 +0700

    feat: Add endpoint for adding films with authentication and authorization

--- src/films/films.controller.ts
+++ src/films/films.controller.ts
@@ -1,9 +1,20 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post, UseGuards } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
+import { Roles } from 'src/common/decorators';
+import { Role } from 'src/common/enums';
+import { AccessTokenGuard, RolesGuard } from 'src/common/guards';
+import { AddFilmDto } from './dto';
 import { FilmsService } from './films.service';
 
 @ApiTags('Films Endpoints')
 @Controller('films')
 export class FilmsController {
   constructor(private readonly filmsService: FilmsService) {}
+
+  @Post('add')
+  @Roles(Role.Admin)
+  @UseGuards(AccessTokenGuard, RolesGuard)
+  async addFilm(@Body() dto: AddFilmDto) {
+    return this.filmsService.addFilm(dto);
+  }
 }
--- src/films/films.service.ts
+++ src/films/films.service.ts
@@ -1,4 +1,22 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { AddFilmDto } from './dto';
 
 @Injectable()
-export class FilmsService {}
+export class FilmsService {
+  constructor(private readonly prisma: PrismaService) {}
+
+  async addFilm(dto: AddFilmDto) {
+    dto.releaseDate = new Date(dto.releaseDate);
+    dto.finishDate = dto.finishDate
+      ? new Date(dto.finishDate)
+      : new Date(dto.releaseDate);
+    dto.finishDate.setMonth(dto.finishDate.getMonth() + 3);
+
+    await this.prisma.films.create({
+      data: {
+        ...dto,
+      },
+    });
+  }
+}
